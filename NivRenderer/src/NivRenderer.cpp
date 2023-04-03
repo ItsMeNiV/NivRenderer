a9d9e3be54ad56a660873c17bd5f196a50abd039
Yannik Hodel <yannik.hodel@gmail.com>
2023-04-03 21:44:37 +0200

    Initialize project

---
+++ NivRenderer/src/NivRenderer.cpp
@@ -0,0 +1,12 @@
+﻿// NivRenderer.cpp : Defines the entry point for the application.
+//
+
+#include "NivRenderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
